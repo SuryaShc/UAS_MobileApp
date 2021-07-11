d731e084d0af21c1887076b6ab69190b0c5a1186
SuryaShc <suryashc.pixel@gmail.com>
2021-07-11 14:28:02 +0700

    new react native project

--- PIN_IT/App.js
+++ PIN_IT/App.js
@@ -1,58 +1,112 @@
-import React, { useEffect } from 'react';
-import { createStackNavigator} from '@react-navigation/stack';
-import { NavigationContainer,useNavigation } from '@react-navigation/native';
-import SplashScreen from './screens/SplashScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import LoginScreen from './screens/LoginScreen';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-import OnBoarding1 from './screens/OnBoarding1';
-import OnBoarding2 from './screens/OnBoarding2';
-import OnBoarding3 from './screens/OnBoarding3';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-const Stack = createStackNavigator();
-
-
-export default Router = () =>{
-  return(
-    <NavigationContainer>
-        
-        <Stack.Navigator >
-          <Stack.Screen 
-              name="SplashScreen" 
-              component={SplashScreen} 
-              options={{
-                  headerShown:false,
-              }}
-          />
-          <Stack.Screen 
-              name="WelcomeScreen" 
-              component={WelcomeScreen} 
-              options={{
-                  headerShown:false,
-              }}
-          />
-          <Stack.Screen 
-              name="Login" 
-              component={LoginScreen} 
-              options={{
-                  headerShown:false,
-              }}
-          />
-          
-        <Stack.Screen name="Boarding1" component={OnBoarding1} options={{
-          headerShown: false
-        }} />
-        <Stack.Screen name="Boarding2" component={OnBoarding2} options={{
-          headerShown: false
-        }} />
-        <Stack.Screen name="Boarding3" component={OnBoarding3} options={{
-          headerShown: false
-        }} />
-
-        </Stack.Navigator>
-        
-    </NavigationContainer>
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
-  
-  }
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
